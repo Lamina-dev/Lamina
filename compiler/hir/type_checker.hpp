694d29128d64feae431060e927dad971c5c9e20c
geguj <geguji@laminasys.org>
2026-08-07 11:15:29 +0800

    更改管道运算符行为

--- compiler/hir/type_checker.hpp
+++ compiler/hir/type_checker.hpp
@@ -25,7 +25,7 @@ class TypeCkContext {
 
     std::optional<Scope::Var *> find_global(const std::string &name) noexcept;
 
-    void new_var(std::string name, std::shared_ptr<Type> type, Scope *scope, bool is_mut = false) noexcept;
+    static void new_var(std::string name, std::shared_ptr<Type> type, Scope *scope, bool is_mut = false) noexcept;
     void new_cur_scope_var(std::string name, std::shared_ptr<Type> type, bool is_mut = false) noexcept;
     void new_global_var(std::string name, std::shared_ptr<Type> type, bool is_mut = false) noexcept;
 
